2ca085f33e796816dfe5db7c31535e8993afd6a1
Smalllong <898310778@qq.com>
2022-06-04 02:36:16 +0800

    use global terser cli

--- compress.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const terser = require("terser")
-
-const src = fs.readFileSync('dist/iife.js').toString()
-
-const minified = terser.minify(src).code
-
-fs.writeFileSync('dist/lightue.min.js', minified)
