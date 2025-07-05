4486369467a1160cc7458c9312b3ef9d194dd460
Smalllong <898310778@qq.com>
2025-07-06 03:50:25 +0800

    fix doc

--- publish-doc.js
+++ publish-doc.js
@@ -1,7 +1,8 @@
-var packageJson = require('./package.json'),
-  fs = require('fs'),
-  install = fs.readFileSync('docs/guide/install.md', { encoding: 'utf-8' }),
-  installZh = fs.readFileSync('docs/zh/guide/install.md', { encoding: 'utf-8' })
+import fs from 'fs'
+import packageJson from './package.json' assert {type: 'json'}
+
+const install = fs.readFileSync('docs/guide/install.md', { encoding: 'utf-8' })
+const installZh = fs.readFileSync('docs/zh/guide/install.md', { encoding: 'utf-8' })
 
 fs.writeFileSync('docs/guide/install.md', install.replace('{{version}}', packageJson.version))
 fs.writeFileSync('docs/zh/guide/install.md', installZh.replace('{{version}}', packageJson.version))
